a396500f3d5c70438e6e74e05f397b16d98e52e4
raja <raja.rakoto7@gmail.com>
2024-02-28 21:33:58 +0300

    src: init entry point + core -> install

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,30 @@
 /* libs */
 import inquirer from 'inquirer';
-/* examples */
-import { dev_prompt } from './examples/list';
-import { pizza_prompt } from './examples/checkbox';
-import { expand_prompt } from './examples/expand';
-import { tvShowLists, recursive_prompt } from './examples/confirm';
-import { complex_prompt } from './examples/complex';
+/* menu */
+import { menu_prompt } from './menu';
+/* core */
+// import { install } from './core/install';
+/* extras */
+import { bannerRenderer } from '../utils/ascii';
 
 // ==============================
 
-// type = input | list
-async function askDev(): Promise<void> {
-  const dev_answers = await inquirer.prompt(dev_prompt);
-  console.log(`Hello ${dev_answers.name}! You prefer ${dev_answers.language}.`);
-}
-
-// type = checkbox
-async function askPizza(): Promise<void> {
-  const pizza_answers = await inquirer.prompt(pizza_prompt);
-  console.dir(pizza_answers);
-}
-
-// type = expand
-async function askExpand(): Promise<void> {
-  const expand_answers = await inquirer.prompt(expand_prompt);
-  console.dir(expand_answers);
-}
-
-// type = input | confirm
-async function askRecursive(): Promise<void> {
-  const recursive_answers = await inquirer.prompt(recursive_prompt);
-  tvShowLists.push(recursive_answers.tvShow);
-  if (recursive_answers.askAgain) {
-    askRecursive();
-  } else {
-    console.log('tvShow lists: ', tvShowLists.join(', '));
-    await askComplex();
+/**
+ * @description Entry point of the CLI
+ */
+async function plumCLI(): Promise<void> {
+  // show banner
+  try {
+    const banner = await bannerRenderer('plum-cli');
+    console.log(`${banner}\n`);
+  } catch (error) {
+    console.error('Error: ASCII banner rendering failed !');
+    console.dir(error);
   }
+
+  // start menu
+  const menu_answers = await inquirer.prompt(menu_prompt);
+  console.dir(menu_answers);
 }
 
-// type = input | password | number | list | confirm | checkbox | rawlist | editor
-async function askComplex(): Promise<void> {
-  const complex_answers = await inquirer.prompt(complex_prompt);
-  console.dir(complex_answers);
-}
-
-async function main(): Promise<void> {
-  await askDev();
-  await askPizza();
-  await askExpand();
-  await askRecursive(); // include askComplex()
-}
-
-main();
+plumCLI();
