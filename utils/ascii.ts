8adeb27436bb45cfedd4f1d0fd1a56d34d7dfaab
raja <raja.rakoto7@gmail.com>
2024-02-28 21:33:06 +0300

    utils: pre valid bannerRenderer() using figlet ASCII art

---
+++ utils/ascii.ts
@@ -0,0 +1,25 @@
+import figlet from 'figlet';
+import chalk from 'chalk';
+import pkg from '../package.json';
+
+// ==============================
+
+/**
+ * @description A function that renders a text as a banner using figlet ASCII art
+ * @param text The text to render
+ * @returns A promise that resolves to the rendered text
+ */
+export function bannerRenderer(text: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    figlet(text, function (err, rendered) {
+      if (err) {
+        reject(err);
+      } else {
+        const packageVersion = pkg.version;
+        const coloredBanner = chalk.magenta(rendered);
+        const result = `${coloredBanner}\n version: ${packageVersion}`;
+        resolve(result);
+      }
+    });
+  });
+}
