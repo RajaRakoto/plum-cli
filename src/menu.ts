2c56e3bfaca7ca08de0b409e49225ea6ad284eb7
raja <raja.rakoto7@gmail.com>
2024-02-29 20:37:48 +0300

    src: set pageSize to 15 -> menu

--- src/menu.ts
+++ src/menu.ts
@@ -10,6 +10,7 @@ export const menu_prompt = [
     name: 'menu',
     message: chalk.green('What do you want to do ...'),
     loop: false,
+    pageSize: 15,
     choices: [
       new inquirer.Separator('============ packages manager ============'),
       {
