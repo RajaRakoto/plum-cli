5f7aa0121553dd8ba7851ba61de4a4f21afc2098
raja <raja.rakoto7@gmail.com>
2024-02-29 17:19:48 +0300

    src: pre valid install() -> packageInstaller()

--- src/core/install.ts
+++ src/core/install.ts
@@ -1,10 +1,23 @@
 /* libs */
 import inquirer from 'inquirer';
 import chalk from 'chalk';
+import * as emoji from 'node-emoji';
 /* index */
 import { plumCLI } from '..';
-
+/* extras */
+import { pkgInstaller } from '../../utils/pkg';
+/* types */
+interface I_install_answers {
+  inProject: boolean;
+  pkgManager: string;
+}
 // ==============================
+
+const plumPackageName = '@rajarakoto/plum';
+const testMode = true;
+const packageLists = ['npm', 'yarn', 'pnpm', 'bun'].map((pkg) => {
+  return `${emoji.get('package')} ${pkg}`;
+});
 
 const install_prompt = [
   {
@@ -20,31 +33,59 @@ const install_prompt = [
     name: 'pkgManager',
     message: chalk.green('Install plum package with:'),
     loop: true,
-    choices: ['npm', 'yarn', 'pnpm', 'bun'],
+    choices: [
+      {
+        name: packageLists[0],
+        value: 'npm',
+      },
+      {
+        name: packageLists[1],
+        value: 'yarn',
+      },
+      {
+        name: packageLists[2],
+        value: 'pnpm',
+      },
+      {
+        name: packageLists[3],
+        value: 'bun',
+      },
+    ],
     default: 'npm',
+    when: (answers: I_install_answers): boolean => answers.inProject,
   },
 ];
 
 export async function install(): Promise<void> {
   const install_answers = await inquirer.prompt(install_prompt);
   if (install_answers.inProject) {
-    console.log('start installation ...');
+    console.log(`start installation, please wait ${emoji.get('wink')} ...`);
 
     switch (install_answers.pkgManager) {
       case 'npm':
-        console.log('npm selected !');
+        testMode
+          ? pkgInstaller('npm')
+          : pkgInstaller('npm', true, plumPackageName);
         break;
       case 'yarn':
-        console.log('yarn selected !');
+        testMode
+          ? pkgInstaller('yarn')
+          : pkgInstaller('yarn', true, plumPackageName);
         break;
       case 'pnpm':
-        console.log('pnpm selected !');
+        testMode
+          ? pkgInstaller('pnpm')
+          : pkgInstaller('pnpm', true, plumPackageName);
         break;
       case 'bun':
-        console.log('bun selected !');
+        testMode
+          ? pkgInstaller('bun', true)
+          : pkgInstaller('bun', true, plumPackageName);
         break;
       default:
-        console.log('npm selected !');
+        testMode
+          ? pkgInstaller('npm')
+          : pkgInstaller('npm', true, plumPackageName);
         break;
     }
   } else {
