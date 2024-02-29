392d5da2e74bd2cc7e405f5c50dc4112527b32c3
raja <raja.rakoto7@gmail.com>
2024-02-29 17:15:59 +0300

    utils: refact | add dev arg

--- utils/installer.ts
+++ utils/installer.ts
@@ -7,15 +7,19 @@ import * as emoji from 'node-emoji';
 /**
  * @description A function that installs a package using a package manager of choice (npm, yarn, pnpm, bun)
  * @param pkgManager The package manager to use
+ * @param dev Whether to install the package as a development dependency (default: false)
  * @param pkg The package to install (default: @nlekane/dummy-npm-package)
  */
 export async function pkgInstaller(
   pkgManager: string,
-  pkg?: string,
+  dev: boolean = false,
+  pkg: string = '@nlekane/dummy-npm-package',
 ): Promise<void> {
+  const commandOptions = dev ? ['--dev'] : [];
+  const installCMD = pkgManager === 'yarn' ? 'add' : 'install';
+
   try {
-    const pkgTest = '@nlekane/dummy-npm-package';
-    await execa(pkgManager, ['install', pkg || pkgTest]);
+    await execa(pkgManager, [installCMD, ...commandOptions, pkg]);
     console.log(chalk.green(`Installation completed ${emoji.get('grin')} !`));
   } catch (error) {
     console.error('Error during installation:', error);
