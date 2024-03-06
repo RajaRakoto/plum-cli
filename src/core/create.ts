c979c83b5d57c8bf08c5e63a0442541f7299d076
raja <raja.rakoto7@gmail.com>
2024-03-06 18:05:27 +0300

    src: init create -> core

---
+++ src/core/create.ts
@@ -0,0 +1,57 @@
+/* libs */
+import inquirer from 'inquirer';
+import { execa } from 'execa';
+import chalk from 'chalk';
+
+// ==============================
+
+const create_prompt = [
+  {
+    type: 'confirm',
+    name: 'create',
+    message: chalk.yellow(
+      'No project found, do you want to create a new one ?',
+    ),
+    default: true,
+  },
+];
+
+export async function create(pkgManager: string): Promise<boolean> {
+  const create_answers = await inquirer.prompt(create_prompt);
+
+  if (create_answers.create) {
+    try {
+      let pkgCMD: string = '';
+      const pkgArgs: string[] = ['init'];
+
+      switch (pkgManager) {
+        case 'npm':
+          pkgCMD = 'npm';
+          pkgArgs.push('-y');
+          break;
+        case 'yarn':
+          pkgCMD = 'yarn';
+          pkgArgs.push('-y');
+          break;
+        case 'pnpm':
+          pkgCMD = 'pnpm';
+          break;
+        case 'bun':
+          pkgCMD = 'bun';
+          pkgArgs.push('-y');
+          break;
+        default:
+          console.error('\n\nError: Unknown package manager');
+          return false;
+      }
+
+      await execa(pkgCMD, pkgArgs);
+      return true;
+    } catch (error) {
+      console.error(`Error during ${pkgManager} init:`, error);
+      return false;
+    }
+  } else {
+    return false;
+  }
+}
