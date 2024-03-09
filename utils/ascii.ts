4edae6f403db046d3603851e3dea395ac9f0472f
raja <raja.rakoto7@gmail.com>
2024-03-09 17:40:17 +0300

    utils: fix bannerRenderer -> win32

--- utils/ascii.ts
+++ utils/ascii.ts
@@ -2,28 +2,35 @@
 import figlet from 'figlet';
 import chalk from 'chalk';
 import * as emoji from 'node-emoji';
+import fs from 'fs';
 /* files */
 import pkg from '../package.json';
+/* utils */
+import { resolveRealPath } from './extras';
 
 // ==============================
+
+const font = fs.readFileSync(resolveRealPath('./fonts/Standard.flf'), 'utf8');
+figlet.parseFont('StandardFont', font);
 
 /**
  * @description A function that renders a text as a banner using figlet ASCII art
  * @param text The text to render
  * @returns A promise that resolves to the rendered text
  */
-export function bannerRenderer(text: string): Promise<string> {
-  return new Promise((resolve, reject) => {
-    figlet(text, function (err, rendered) {
-      if (err) {
-        reject(err);
-      } else {
-        const coloredBanner = chalk.magenta(rendered);
-        const packageVersion = pkg.version;
-        const description = `Customize your website faster with PLUM, a mixins toolset powered by SASS. Quickly produce consistent, scalable CSS output, regardless of project size ${emoji.get('purple_heart')}`;
-        const result = `${coloredBanner}\n ${chalk.underline('version:')} ${packageVersion}\n\n ${description}`;
-        resolve(result);
-      }
+export async function bannerRenderer(text: string): Promise<string> {
+  try {
+    const rendered = await figlet.textSync(text, {
+      font: 'StandardFont' as figlet.Fonts,
     });
-  });
+    const coloredBanner = chalk.magenta(rendered);
+    const packageVersion = pkg.version;
+    const description = `Customize your website faster with PLUM, a mixins toolset powered by SASS. Quickly produce consistent, scalable CSS output, regardless of project size ${emoji.get('purple_heart')}`;
+    const result = `${coloredBanner}\n ${chalk.underline('version:')} ${packageVersion}\n\n ${description}`;
+    return result;
+  } catch (error) {
+    console.error('An error occurred while rendering the banner:', error);
+    console.dir(error);
+    return '';
+  }
 }
