f8bb6d696eef4f0584d82c322b10adce03bd413e
raja <raja.rakoto7@gmail.com>
2024-03-29 21:50:34 +0300

    src: add constants struct | refact

---
+++ src/constants/index.ts
@@ -0,0 +1,15 @@
+export const DEVMODE = false;
+export const PLUM_PACKAGENAME = '@raja-rakoto/plum';
+export const SCRIPTS_PATH = './scripts/';
+export const SCRIPTS_DEFAULT_FOLDER = './plum-scripts';
+export const PATH_LISTS = {
+  animate: './apps/animate/index.html',
+  buttons2: './apps/buttons2/index.html',
+  flex: './apps/flex/index.html',
+  grid: './apps/grid/index.html',
+  hover2: './apps/hover2/index.html',
+  magic: './apps/magic/index.html',
+  buttons: './apps/buttons.html',
+  'oc-scheme': './apps/oc-scheme.png',
+  shadows: './apps/shadows.html',
+};
